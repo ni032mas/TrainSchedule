db0da08c96d68b4671234ca5c767efaf4f120b3a
ni032mas <ni032mas@mail.ru>
2024-09-15 20:29:54 +0300

    Added dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlinx.serialization)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
